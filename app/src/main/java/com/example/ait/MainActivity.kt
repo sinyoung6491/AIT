d581ce0e3e4ab6840c3792fda3bd15e80db9c8b8
Sinyoung Kang <sinyoung6491@gmail.com>
2023-08-17 15:54:31 +0900

    [add]settings

--- app/src/main/java/com/example/ait/MainActivity.kt
+++ app/src/main/java/com/example/ait/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.ait
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ait.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
